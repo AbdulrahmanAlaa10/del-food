c0b3ec27cf31c1d60ccf0933d94fa2aa6694a8f6
alaaboody65 <alaaboody65@gmail.com>
2020-12-11 18:05:44 +0200

    first-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./Store/Store";
 import './App.css';
+// page
+import Home from "./Home/index";
+import Search from "./Search";
+import OnlineProduct from "./OnlineProduct/index";
+import Cart from "./Cart/index";
+import Food from "./Food/index";
+import CartIcon from "./CartIcon/CatrIcon"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+ 
+  render() {
+    return (
+      <Router>
+
+<div className="container">
+<div className="navnvbar">
+  <div className="navbar">
+
+  <Link to="/" className="bar">Delivery^_^</Link>
+
+      <div className="links">
+      <Link to="/Search" className="barr">Search</Link>
+        <Link to="/Food" className="barr">Food</Link>
+        <Link to="/Cart" className="barr">Cart</Link>
+
+      </div>
+      <CartIcon />
+
     </div>
-  );
+  </div>
+</div>
+
+
+<Route path="/" component={Home} exact />
+<Route path="/Food" component={Food} exact />
+<Route path="/Food/:id" component={OnlineProduct} exact />
+<Route path="/Cart" component={Cart} exact />
+<Route path="/Search" component={Search} exact />
+
+</Router>
+
+    );
+  }
 }
 
-export default App;
+function AppWithStore(){
+  return <Provider store={store}>
+    <App />
+  </Provider>
+}
+
+export default AppWithStore;
